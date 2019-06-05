38a19751657191cebf0176565eb28076e93f8cf5
David Govea <govea.d@gmail.com>
2019-06-05 11:34:43 -0700

    add initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export * from './lib/number';
+import { fiber } from 'posterus/fiber';
+import { useEffect } from 'react';
+
+function useTask(taskFn: IterableIterator<any>): void {
+  useEffect(() => {
+    const future = fiber((taskFn as any)());
+    return () => {
+      future.deinit();
+    }
+  });
+}
+
+export default useTask;
