0ab94ca651045495ce429d2450c7381ab55debd0
David Govea <govea.d@gmail.com>
2019-06-06 00:33:32 -0700

    keep refining posterus types

--- src/types/posterus.d.ts
+++ src/types/posterus.d.ts
@@ -1,19 +1,32 @@
 declare namespace Posterus {
-  export class Future {
-    static from: (...args: any[]) => Future;
-    static fromResult: (...args: any[]) => Future;
-    static all: (...args: any[]) => Future;
-    static race: (...args: any[]) => Future;
-    map: (...args: any[]) => any;
-    mapResult: (...args: any[]) => any;
-    mapError: (...args: any[]) => any;
-    finally: (...args: any[]) => any;
-    deinit: (...args: any[]) => any;
+  export class Future<T = any> {
+    map: (mapper: (error: any, result: any) => any) => Future;
+    mapResult: (mapper: (result: any) => any) => Future;
+    mapError: (mapper: (error: any) => any) => Future;
+    finally: (mapper: (error: any, result: any) => any) => Future;
+    deinit: () => void;
+    weak: () => Future<T>;
+    settle: (error: any, result: any) => void;
+    toPromise: () => Promise<T>;
+    then: Promise<T>['then'];
+    catch: Promise<T>['catch'];
+    finishPending: (...args: any[]) => any;
+    deref: () => T | undefined;
+  }
+
+  interface FutureConstructor {
+    readonly prototype: Future<any>;
+    new <T>(): Future<T>;
+    from: (...args: any[]) => Future;
+    fromResult: (result?: any) => Future;
+    fromPromise: (promise: Promise<any>) => Future<any>;
+    all: (values: any[]) => Future;
+    race: (values: any[]) => Future;
   }
 }
 
 declare module 'posterus' {
-  export const Future: Posterus.Future;
+  export const Future: Posterus.FutureConstructor;
 }
 
 declare module 'posterus/fiber' {
