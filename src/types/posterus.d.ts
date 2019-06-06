d8f129da3584935f808f2358d10fcb30a7b9ff0f
David Govea <govea.d@gmail.com>
2019-06-06 12:35:07 -0700

    more specific types for posterus

--- src/types/posterus.d.ts
+++ src/types/posterus.d.ts
@@ -1,34 +1,39 @@
 declare namespace Posterus {
   export class Future<T = any> {
-    map: (mapper: (error: any, result: any) => any) => Future;
-    mapResult: (mapper: (result: any) => any) => Future;
-    mapError: (mapper: (error: any) => any) => Future;
-    finally: (mapper: (error: any, result: any) => any) => Future;
+    map: <U>(mapper: (error: any, result?: T) => any) => Future<U>;
+    mapResult: <U>(mapper: (result: T) => any) => Future<U>;
+    mapError: <U>(mapper: (error: any) => any) => Future<U>;
+    finally: <U>(mapper: (error: any, result: any) => any) => Future<U>;
     deinit: () => void;
     weak: () => Future<T>;
     settle: (error: any, result: any) => void;
     toPromise: () => Promise<T>;
     then: Promise<T>['then'];
     catch: Promise<T>['catch'];
-    finishPending: (...args: any[]) => any;
+    finishPending: () => void;
     deref: () => T | undefined;
   }
 
   interface FutureConstructor {
     readonly prototype: Future<any>;
     new <T>(): Future<T>;
-    from: (...args: any[]) => Future;
-    fromResult: (result?: any) => Future;
-    fromPromise: (promise: Promise<any>) => Future<any>;
-    all: (values: any[]) => Future;
-    race: (values: any[]) => Future;
+    from: (...args: any[]) => Future<any>;
+    fromResult: <U>(result: U) => Future<U>;
+    fromError: <U>(error: any) => Future<U>;
+    fromPromise: <U>(promise: Promise<U>) => Future<U>;
+    all: (values: any[]) => Future<any>;
+    race: (values: any[]) => Future<any>;
   }
 }
 
 declare module 'posterus' {
   export const Future: Posterus.FutureConstructor;
+  export const isFuture: (value: any) => boolean;
+  export const isDeinitError: (error: any) => boolean;
 }
 
 declare module 'posterus/fiber' {
-  export const fiber: (...args: any[]) => Posterus.Future;
+  // Keep an eye on https://github.com/Microsoft/TypeScript/pull/30790
+  //  would like to remove the <any> and infer from iterator's explicit return type
+  export const fiber: <T>(iterator: IterableIterator<any>) => Posterus.Future<T>;
 }
